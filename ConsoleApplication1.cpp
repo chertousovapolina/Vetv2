171fd7b8c446d8130db32948e8b9e3ebeddad7f3
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-10-30 17:09:17 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+﻿// Чертоусова Ветвления
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, n;
+	cin >> a >> b >> n;
+	if (a<=n && n<=b)
+	{
+		cout << "YES";
+	}
+	else { cout << "NO"; }
+}
